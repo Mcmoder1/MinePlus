e2968b8d3e1bcc9dc7f3a05aeed19529e2c33b68
hussa <hussam.owhaib2009@gmail.com>
2022-07-25 17:21:15 +0400

    my new mod

---
+++ src/main/java/net/moder0/mineplus/MinePlusClient.java
@@ -0,0 +1,10 @@
+package net.moder0.mineplus;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MinePlusClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
